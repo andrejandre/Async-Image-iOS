2a70c0001b764b19a464096a413eb4141d9ccc4e
Andre Unsal <andreunsal@gmail.com>
2023-01-15 09:37:19 -0500

    Initial commit

---
+++ Async Image iOS/Async Image iOS/Async_Image_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Image_iOSApp.swift
+//  Async Image iOS
+//
+//  Created by Andre Unsal on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Async_Image_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Async Image iOS/Async Image iOSUITests/Async_Image_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Async_Image_iOSUITestsLaunchTests.swift
+//  Async Image iOSUITests
+//
+//  Created by Andre Unsal on 1/15/23.
+//
+
+import XCTest
+
+final class Async_Image_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
